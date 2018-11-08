720390bca906652cb09a4f80e3f58c98680cd700
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2018-11-08 13:47:48 +0100

    Removed reference code

--- index.php
+++ index.php
@@ -17,37 +17,11 @@ require __DIR__.'/header.php';
          <span></span>
        </div>
        <a href="#contact">Contact</a>
-
      </div>
 
     <!--/END NAVBAR-->
-    <header class="hero-header">
-      <p class="hero-text">You are special</p>
-      <p class="hero-text">Fast Security, FS.1.0</p>
-    </header>
-    <footer class="hero-footer">
-      <a class="button button-primary" href="#">button one</a>
-      <a class="button" href="#">button two</a>
-    </footer>
   </section>
   <!--/END HERO-->
-
-  <section>
-    <div class="div1">
-      <div class="box">
-        box
-      </div>
-      <div class="box">
-        box2
-      </div>
-    </div>
-  </section>
-  <section>
-    <div class="div2">
-      box1
-    </div>
-  </section>
-
  <?php
  require __DIR__.'/footer.php';
   ?>
