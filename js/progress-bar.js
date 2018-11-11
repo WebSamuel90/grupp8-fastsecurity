feeac89c513294521a0016419a701042b67f5d11
Izabella <izzwii.izabella@gmail.com>
2018-11-11 20:58:55 +0100

    fixed opacity on background

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -9,11 +9,22 @@
 //   var position = scrollPercent;
 //
 // })
-
-
-
+//
+//
+//
 // $(window).scroll(function () {
 //
 //    $("#progressbar").attr('value', position);
 //
 // });
+
+$(window).scroll(function () {
+  var s = $(window).scrollTop(),
+        d = $(document).height(),
+        c = $(window).height();
+        scrollPercent = (s / (d-c)) * 100;
+        var position = scrollPercent;
+
+   $("#progressbar").attr('value', position);
+
+});
