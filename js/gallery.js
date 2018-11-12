e7f0d46b054914dfbbd72636e5b71111360f2e1c
Samuel <sumlebulle@hotmail.com>
2018-11-12 12:54:04 +0100

    Changed gallary from background images to img tags. Added a change image eventlistener

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,16 @@
-const picTwo = document.querySelector('.picture-two');
-console.log(picTwo);
 
-picTwo.addEventListener('click', () => {
-  picTwo.classList.remove('small')
-  picTwo.classList.add('big');
-  console.log(picTwo);
+
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach(function (box) {
+  box.addEventListener('click', function () {
+
+    let boxes2 = document.querySelectorAll(".box");
+
+      for(i = 0; i < boxes2.length; i++){
+        boxes2[i].classList.remove('big');
+      }
+
+      box.classList.add('big')
+  })
 })
