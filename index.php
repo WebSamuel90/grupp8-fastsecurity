f1bf01d41a13f8ede223a3b6367244ea8eaf04c8
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2018-11-22 21:17:31 +0100

    Adjusted Mobile logo size

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ require __DIR__.'/header.php';
    <!--MOBILE NAVBAR-->
    <div class="mobile-logo">
      <p>MENU</p>
-     <img src="images/icons_menu_arrow.svg" alt="Fast Security Logo" width="50px;">
+     <img src="images/icons_menu_arrow.svg" alt="Fast Security Logo" width="35px;">
    </div>
    <div class="mobile-navbar-container">
      <div class="mobile-navbar-left">
