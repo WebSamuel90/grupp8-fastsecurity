c43a351d46cded2c422799612e225730658e72f6
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2018-11-22 21:44:58 +0100

    Added javascript to make the mobile navbar work

--- footer.php
+++ footer.php
@@ -2,5 +2,8 @@
     <script type="text/javascript" src="js/progress-bar.js"></script>
     <script type="text/javascript" src="js/gallery.js"></script>
     <script type="text/javascript" src="js/details-pop-up.js"></script>
+    <script type="text/javascript" src="js/mobile-navbar-popup.js">
+
+    </script>
   </body>
 </html>
---
+++ js/mobile-navbar-popup.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const mobileNavbarContainer = document.querySelector('.mobile-navbar-container');
+const mobileLogo = document.querySelector('.mobile-logo');
+const mobileNavbarPopup = document.querySelector('.mobile-navbar-popup');
+
+mobileLogo.addEventListener('click', ()=>{
+  mobileNavbarContainer.classList.toggle('mobile-navbar-popup');
+
+})
