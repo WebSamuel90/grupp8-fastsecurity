9419e2c5fcc508c7c4cd69d6badffe21e5d584bc
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2018-11-28 18:05:29 +0100

    Adjusted the mobile  menu heights

--- js/mobile-navbar-popup.js
+++ js/mobile-navbar-popup.js
@@ -2,7 +2,7 @@
 
 const mobileNavbarContainer = document.querySelector('.mobile-navbar-container');
 const mobileLogo = document.querySelector('.mobile-logo');
-const mobileNavbarPopup = document.querySelector('.mobile-navbar-popup');
+// const mobileNavbarPopup = document.querySelector('.mobile-navbar-popup');
 
 mobileLogo.addEventListener('click', ()=>{
   mobileNavbarContainer.classList.toggle('mobile-navbar-popup');
