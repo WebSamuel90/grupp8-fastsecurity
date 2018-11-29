4d4daf416f6f27e373e3965ff148e883968e9613
Samuel <sumlebulle@hotmail.com>
2018-11-29 22:02:31 +0100

    Fixed mobile slider

--- js/gallery-slider.js
+++ js/gallery-slider.js
@@ -5,3 +5,11 @@ $(document).ready(function(){
       mobileFirst: true
   });
 });
+
+$(document).ready(function(){
+  $('.slider-mobile').slick({
+      prevArrow: '<img class="slide-left" src="images/slider_arrow_mobile.svg">',
+      nextArrow: '<img class="slide-right" src="images/slider_arrow_mobile.svg">',
+      mobileFirst: true
+  });
+});
